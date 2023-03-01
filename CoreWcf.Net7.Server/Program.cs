3abac26fbba80ede08fd6c52d8519c31f2cae7aa
Björn Hellander <bjorn.hellander@gmail.com>
2023-03-01 20:23:13 +0100

    Moved service configuration code into a separate class

--- CoreWcf.Net7.Server/Program.cs
+++ CoreWcf.Net7.Server/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreWCF;
-using CoreWCF.Configuration;
-using Interface;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Unity;
 using WcfTest.Interface;
 
@@ -27,64 +20,17 @@ namespace WcfTest.CoreWcf.Server
 
                 Console.WriteLine("Starting services...");
                 var services = container.ResolveAll<IHostableService>().ToList();
-                var webHostBuilder = CreateWebHostBuilder(ServiceInformation.Port, services);
-                var host = webHostBuilder.Build();
-                await host.StartAsync();
+                var serviceStarter = new ServiceStarter(ServiceInformation.Port, services);
+                serviceStarter.Start();
 
                 Console.WriteLine("Press any key to exit");
                 Console.ReadKey();
-                await host.StopAsync();
+                await serviceStarter.StopAsync();
             }
             catch (CommunicationException e)
             {
                 Console.WriteLine($"Caught exception: {e}");
             }
-        }
-
-        private static IWebHostBuilder CreateWebHostBuilder(int portNumber, IReadOnlyList<IHostableService> services)
-        {
-            var webHostBuilder = WebHost.CreateDefaultBuilder()
-                .UseKestrel(kestrelServerOptions =>
-                {
-                    kestrelServerOptions.ListenAnyIP(0); // 0 means any available port
-                })
-                .UseNetTcp(portNumber)
-                .ConfigureServices(serviceCollection =>
-                {
-                    serviceCollection.AddServiceModelServices();
-                    foreach (var service in services)
-                    {
-                        serviceCollection.AddSingleton(service.GetType(), service);
-                    }
-                })
-                .Configure(applicationBuilder =>
-                 {
-                     applicationBuilder.UseServiceModel(serviceBuilder =>
-                     {
-                         foreach (var service in services)
-                         {
-                             serviceBuilder.AddService(service.GetType());
-
-                             foreach (var serviceContractType in service.ServiceContracts)
-                             {
-                                 var binding = new NetTcpBinding();
-                                 var address = EndpointAddressFactory.Create(serviceContractType);
-                                 serviceBuilder.AddServiceEndpoint(
-                                     service.GetType(),
-                                     serviceContractType,
-                                     binding,
-                                     address,
-                                     listenUri: null);
-                             }
-                         }
-                     });
-                 })
-                .ConfigureLogging((context, logging) =>
-                {
-                    logging.ClearProviders();
-                });
-
-            return webHostBuilder;
         }
     }
 }
---
+++ CoreWcf.Net7.Server/ServiceStarter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreWCF;
+using CoreWCF.Configuration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WcfTest.CoreWcf.Server
+{
+    internal class ServiceStarter
+    {
+        private readonly int port;
+        private readonly List<IHostableService> services;
+        private IWebHost host;
+
+        public ServiceStarter(int port, List<IHostableService> services)
+        {
+            this.port = port;
+            this.services = services;
+        }
+
+        public void Start()
+        {
+            var webHostBuilder = CreateWebHostBuilder(port, services);
+            host = webHostBuilder.Build();
+            host.Start();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(int portNumber, IReadOnlyList<IHostableService> services)
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder()
+                .UseKestrel(kestrelServerOptions =>
+                {
+                    kestrelServerOptions.ListenAnyIP(0); // 0 means any available port
+                })
+                .UseNetTcp(portNumber)
+                .ConfigureServices(serviceCollection =>
+                {
+                    serviceCollection.AddServiceModelServices();
+                    foreach (var service in services)
+                    {
+                        serviceCollection.AddSingleton(service.GetType(), service);
+                    }
+                })
+                .Configure(applicationBuilder =>
+                {
+                    applicationBuilder.UseServiceModel(serviceBuilder =>
+                    {
+                        foreach (var service in services)
+                        {
+                            serviceBuilder.AddService(service.GetType());
+
+                            foreach (var serviceContractType in service.ServiceContracts)
+                            {
+                                var binding = new NetTcpBinding();
+                                var address = EndpointAddressFactory.Create(serviceContractType);
+                                serviceBuilder.AddServiceEndpoint(
+                                    service.GetType(),
+                                    serviceContractType,
+                                    binding,
+                                    address,
+                                    listenUri: null);
+                            }
+                        }
+                    });
+                })
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.ClearProviders();
+                });
+
+            return webHostBuilder;
+        }
+
+        public async Task StopAsync()
+        {
+            await host.StopAsync();
+        }
+    }
+}
